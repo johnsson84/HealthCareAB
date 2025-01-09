52049930291fe12a614906a3cfe0e94495a1f06c
[john gunnarsson] <john.gunnarsson@unipower.se>
2025-01-09 16:37:01 +0100

    setting up test class for validatetoken tests, updatePassword tests, sendPasswordResetLink tests

--- src/test/java/health/care/booking/PasswordResetServiceTest.java
+++ src/test/java/health/care/booking/PasswordResetServiceTest.java
@@ -1,5 +1,37 @@
 package health.care.booking;
 
+import health.care.booking.respository.TokenPasswordResetRepository;
+import health.care.booking.respository.UserRepository;
+import health.care.booking.services.MailService;
+import health.care.booking.services.PasswordResetService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class PasswordResetServiceTest {
+
+    @Mock
+    private MailService mailService;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+
+    @Mock
+    private TokenPasswordResetRepository tokenPasswordResetRepository;
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private PasswordResetService passwordResetService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
 
 }
